6436bb08f137bf9690e06747c986b51f8dc9ff72
Pablo Truffa <truffa12@gmail.com>
2021-09-09 14:16:11 -0300

    Se actualizo http x https en getGifs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,6 @@
+export const getGifs = async(category) => {
 
-    export const getGifs = async(category) => {
-
-        const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=O4NrAlzZ3wXrt5afhbkhyHypjLZdGdmZ`;
+        const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI( category ) }&limit=10&api_key=O4NrAlzZ3wXrt5afhbkhyHypjLZdGdmZ`;
         const resp = await fetch( url );
         const {data} = await resp.json();
         
@@ -13,7 +12,5 @@
             }
         })
 
-        return gifs;
-
-        
-    }
+        return gifs;  
+}
